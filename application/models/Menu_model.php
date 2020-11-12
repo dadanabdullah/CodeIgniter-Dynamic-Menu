1c9c1d7692c47d046bc643facafb790fe19a1838
Dadan Abdullah <hallo.dadan@gmail.com>
2020-11-12 14:41:10 +0700

    Delete unnecessary files

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -1,10 +1,11 @@
-<?php
-class Menu_model extends CI_Model {
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Menu_model extends CI_Model
+{
 
 	public function all()
 	{
-		return $this->db->get("menus")
-					->result_array();
+		return $this->db->get("menus")->result_array();
 	}
 
 }
