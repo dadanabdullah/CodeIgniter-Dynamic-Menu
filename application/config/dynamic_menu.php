0c3e25510c0391ffcd791c8fb668f30f6c4d12e2
Dadan Abdullah <hallo.dadan@gmail.com>
2020-11-12 14:50:50 +0700

    Apply the AdminLTE 2 style

--- application/config/dynamic_menu.php
+++ application/config/dynamic_menu.php
@@ -7,19 +7,25 @@ $config["menu_icon"] = 'icon';
 $config["menu_key"] = 'slug';
 $config["menu_order"] = 'number';
 
-$config["nav_tag_open"] = '<ul>';
+$config["nav_tag_open"] = '<ul class="sidebar-menu" data-widget="tree">';
 $config["nav_tag_close"] = '</ul>';
 
 $config["item_tag_open"] = '<li>'; 
 $config["item_tag_close"] = '</li>';	
 
-$config["parent_tag_open"] = '<li>';	
+$config["parent_tag_open"] = '<li class="treeview">';	
 $config["parent_tag_close"] = '</li>';	
-$config["parent_anchor"] = '<a href="%s">%s</a>';
+$config["parent_anchor"] = '
+	<a href="%s">%s
+		<span class="pull-right-container">
+			<i class="fa fa-angle-left pull-right"></i>
+		</span>
+	</a>
+';
 
 $config["children_tag_open"] = '<ul class="treeview-menu">';
 $config["children_tag_close"] = '</ul>';
 
 // $config['icon_position'] = 'left'; // 'left' or 'right'
 // $config['menu_icons_list'] = array();
-// $config['icon_img_base_url'] = ''; 
+// $config['icon_img_base_url']  ''; 
